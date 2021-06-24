39a4a479bdd6328aa40082e6e23601f90500f771
ZiroxXDev <86424400+ZiroxXDev@users.noreply.github.com>
2021-06-24 19:35:31 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 
 const config = {
-    "TOKEN" : "ODU3NTM5ODI3MDE5OTM5ODYw.YNREMw.Du8rR6iOJHMyokMx4BMMHdxa_bc",
+    "TOKEN" : "ODU3NTM5ODI3MDE5OTM5ODYw.YNREMw.lgXR8bbvCK6pxDK3U7NuB_QCBx4",
     "PREFIX" : "%",
 }
 module.exports = config;
